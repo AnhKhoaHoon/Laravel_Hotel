c96ae14d78586cd83651dea1e080c5f111ecd2a2
John Doe <johndoe@example.com>
2024-05-13 16:22:13 +0700

    Room Check Out Part 4

--- resources/views/frontend/checkout/checkout.blade.php
+++ resources/views/frontend/checkout/checkout.blade.php
@@ -137,28 +137,33 @@
                                     <br>
 
                                     <table class="table" style="width: 100%">
-
-                                          <tr>
-                                                <td><p>Total Night ( 4)</p></td>
-                                                <td style="text-align: right"><p>Room Name</p></td>
-                                          </tr>
-                                          <tr>
+                                        @php
+                                      $subtotal = $room->price * $nights * $book_data['number_of_rooms']; 
+                                      $discount =($room->discount/100)*$subtotal;  
+                                        @endphp
+                                
+                                            <tr>
+                                                <td><p>Total Night <br> <b> ( {{ $book_data['check_in'] }} - {{ $book_data['check_out'] }})</b></p></td>
+                                                <td style="text-align: right"><p> {{ $nights }} Days</p></td>
+                                            </tr>
+                                            <tr>
                                                 <td><p>Total Room</p></td>
-                                                <td style="text-align: right"><p>3</p></td>
-                                          </tr>
-                                          <tr>
+                                                <td style="text-align: right"><p>{{ $book_data['number_of_rooms'] }}</p></td>
+                                            </tr>
+                                            <tr>
                                                 <td><p>Subtotal</p></td>
-                                                <td style="text-align: right"><p>200</p></td>
-                                          </tr>
-                                          <tr>
+                                                <td style="text-align: right"><p>${{ $subtotal }}</p></td>
+                                            </tr>
+                                            <tr>
                                                 <td><p>Discount</p></td>
-                                                <td style="text-align:right"> <p>Discount</p></td>
-                                          </tr>
-                                          <tr>
+                                                <td style="text-align:right"> <p>${{ $discount }}</p></td>
+                                            </tr>
+                                            <tr>
                                                 <td><p>Total</p></td>
-                                                <td style="text-align:right"> <p>Total</p></td>
-                                          </tr>
+                                                <td style="text-align:right"> <p>${{ $subtotal-$discount }}</p></td>
+                                            </tr>
                                     </table>
+                                
 
                               </div>
                         </div>
